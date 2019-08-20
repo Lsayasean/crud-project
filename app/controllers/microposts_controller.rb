b7a3eb5a4ff9f748850e37226d839dcfaa522d82
Lanrna Sayasean <lanrna@bigsouthernsoftware.com>
2019-08-20 12:30:53 -0400

    messing with redirect_to in rails

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,7 +10,7 @@ class MicropostsController < ApplicationController
     def create
         @micropost = Micropost.new(micro_params)
         if @micropost.save
-            redirect_to { action: "index", id: params[:jewel_id] }
+            redirect_back fallback_location: { action: "index", id: params[:jewel_id] }
         else
             redirect_to '/jewels/show'
         end
@@ -19,7 +19,7 @@ class MicropostsController < ApplicationController
     def update
         @micropost = Micropost.find(params[:id])
         if @micropost.update_attributes(micro_params)
-            redirect_to "/comments/#{params[:jewel_id]}"
+            redirect_to '/jewels/show'
         else
             redirect_to '/jewels/show'
         end
